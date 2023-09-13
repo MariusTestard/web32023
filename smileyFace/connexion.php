0f380dea53343a328633221c3fcb18e470ffc7a9
Marius Testard <marius.testard.01@edu.cegeptr.qc.ca>
2023-09-13 14:12:35 -0400

    smileyFace

--- smileyFace/connexion.php
+++ smileyFace/connexion.php
@@ -1,96 +1,110 @@
+<?php
+/*
+session_start();
+*/
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="css/connexion.css">
     <title>connexion.php</title>
 </head>
+
 <body>
-<?php
-    $username = $email = $passwd = "";
+    <?php
+    /*
+    if ($_SERVER["REQUEST_METHOD"] != "POST") {
+        $_SESSION["connexion"] = false;
+    }
+    if ($_SESSION["connexion"] == false) {
+        */
+        //On crée les variables du formulaire vide
+        $numEmplo = $passwd = "";
 
-    //On crée les variables d'erreurs vides
-    $errorUser = $errorEmail = $errorPasswd = "";
+        //On crée les variables d'erreurs vides
+        $errorNumEplo = $errorPasswd = "";
 
-    //La variable qui permet de savoir s'il y a au moins une erreur dans le formulaire
-    $erreur = false;
+        //La variable qui permet de savoir s'il y a au moins une erreur dans le formulaire
+        $erreur = false;
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        //Si on entre, on est dans l'envoie du formulaire 
-        if (empty($_POST['username'])) {
-            $errorUser = "Le nom d'utilisateur est manquant ↑";
-            $erreur = true;
-        } else {
-            $username = test_input($_POST["username"]);
-        }  if (empty($_POST['email'])) {
-            $errorEmail = "L'email est manquant ↑";
-            $erreur = true;
-        } else {
-            $email = test_input($_POST["email"]);
-        } if (empty($_POST['passwd'])) {
-            $errorPasswd = "Le mot de passe est manquant ↑";
-            $erreur = true;
-        } else {
-            $passwd = test_input($_POST["passwd"]);
-        }
-
-        // Inserer dans la base de données
-        if ($erreur != true) {
-            $username = $_POST['username'];
-            $email = $_POST['email'];
-            $passwd = $_POST['passwd'];
-
-            $passwd = sha1($passwd, false);
-
-            $servername = "localhost";
-            $usernameBD = "root";
-            $passwordBD = "root";
-            $bd = "phpbd";
-
-            $conn = new mysqli($servername, $usernameBD, $passwordBD, $bd);
-            if ($conn->connect_error) {
-                die("Connection failed: " . $conn->connect_error);
-            }
-            $sql = "INSERT INTO usagers (id, user, email, passwd, ip, machine) 
-            VALUES (NULL, '$username', '$email', '$passwd', '', '')";
-            $conn->query('SET NAMES utf8');
-            if (mysqli_query($conn, $sql)) {
-                echo "Enregistrement réussi";
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            //Si on entre, on est dans l'envoie du formulaire 
+            if (empty($_POST['numEmploye'])) {
+                $errorNumEplo = "Le numéro d'employé est manquant ↑";
+                $erreur = true;
             } else {
-                echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+                $numEmplo = test_input($_POST["numEmploye"]);
             }
-            header("Location: index.php");
-            mysqli_close($conn); 
+            if (empty($_POST['password'])) {
+                $errorPasswd = "Le mot de passe est manquant ↑";
+                $erreur = true;
+            } else {
+                $passwd = test_input($_POST["password"]);
+            }
+
+            // Inserer dans la base de données
+            if ($erreur != true) {
+                $passwd = $_POST['passsword'];
+                $passwd = sha1($passwd, false);
+
+                $servername = "localhost";
+                $usernameBD = "root";
+                $passwordBD = "root";
+                $bd = "smileyFace";
+
+                $conn = new mysqli($servername, $usernameBD, $passwordBD, $bd);
+                if ($conn->connect_error) {
+                    die("Connection failed: " . $conn->connect_error);
+                }
+
+                $sql = "SELECT * FROM user WHERE numEmploye = '$numEmplo' AND password = '$passwd'";
+                $result = $conn->query($sql);
+
+                if ($result->num_rows > 0) {
+                    $row = $result->fetch_assoc();
+                    echo "<h1>Connecté</h1>";
+                    $_SESSION["connexion"] = true;
+                    //header("Location: index.php");
+                } else {
+                        $erreur = true;
+                        $numEmplo = "";
+                        $errorPasswd = "N° Employé or password invalide";
+                    }
+                    $conn->close();
     ?>
-        <?php
+            <?php
+                }
             }
-        }
-    if ($_SERVER["REQUEST_METHOD"] != "POST" || $erreur == true) {
-        ?>
-        <div class="container-fluid h-100">
-            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-                <div class="form-group">
-                    <label for="username">Username</label>
-                    <input type="text" class="form-control" placeholder="Username" name="username">
-                    <span><?php echo $errorUser; ?></span>
+        if ($_SERVER["REQUEST_METHOD"] != "POST" || $erreur == true) {
+            ?>
+           <div class="container-fluid h-100">
+            <div class="row middle h-100">
+                <div class="col-2">
+                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                        <div class="form-group">
+                            <label for="username">N° Employé</label>
+                            <input type="text" class="form-control" placeholder="N° Employé" name="numEmplo">
+                            <span><?php echo $errorNumEplo; ?></span>
+                        </div>
+                        <div class="form-group">
+                            <label for="passwd">Mot de passe</label>
+                            <input type="password" class="form-control" placeholder="Password" name="passwd">
+                            <span><?php echo $errorPasswd; ?></span>
+                        </div>
+                        <div class="middle">
+                        <button type="submit" class="btn btn-success mt-2 maxlargeur">Se connecter</button>
+                        </div>
+                    </form>
                 </div>
-                <div class="form-group">
-                    <label for="email">Email</label>
-                    <input type="text" class="form-control" placeholder="Email" name="email">
-                    <span><?php echo $errorEmail; ?></span>
-                </div>
-                <div class="form-group">
-                    <label for="passwd">Password</label>
-                    <input type="password" class="form-control" placeholder="Password" name="passwd">
-                    <span><?php echo $errorPasswd; ?></span>
-                </div>
-                <button type="submit" class="btn btn-success mt-2">Create</button>
-            </form>
+            </div>
         </div>
     <?php
-    }
+        }
     function test_input($data)
     {
         $data = trim($data);
@@ -99,5 +113,7 @@
         return $data;
     }
     ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
+
 </html>
